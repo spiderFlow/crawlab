10bb511c5f30a0382c0fc1ca781df9e603c1dffa
Marvin Zhang <tikazyq@163.com>
2024-11-05 11:40:12 +0800

    feat: updated dependency handler logic

--- core/models/models/dependency_log.go
+++ core/models/models/dependency_log.go
@@ -1,10 +1,7 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type DependencyLog struct {
 	any                      `collection:"dependency_logs"`
 	BaseModel[DependencyLog] `bson:",inline"`
-	TaskId                   primitive.ObjectID `json:"task_id" bson:"task_id"`
-	Content                  string             `json:"content" bson:"content"`
+	Content                  string `json:"content" bson:"content"`
 }
