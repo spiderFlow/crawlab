98a9c907c9cbcbd3d28dab33ca4f2d36e70d79ef
Marvin Zhang <tikazyq@163.com>
2024-11-19 17:40:44 +0800

    fix: test case issue

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/crawlab-team/crawlab/core/cmd"
+	"github.com/crawlab-team/crawlab/core/config"
 	"github.com/crawlab-team/crawlab/core/utils"
 )
+
+func init() {
+	config.InitConfig()
+}
 
 func main() {
 	go func() {
