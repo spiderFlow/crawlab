cd2abe954788e579fe1e51a3c841d31948fcef5e
Marvin Zhang <tikazyq@163.com>
2024-11-22 20:44:26 +0800

    ci: updated workflow

--- docker/base-image/install/java/java.sh
+++ docker/base-image/install/java/java.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-# Exit on error
-set -e
+version="11.0.12-open"
 
-# Update package list and install OpenJDK 11
-DEBIAN_FRONTEND=noninteractive apt-get update && \
-    apt-get install -y --no-install-recommends openjdk-11-jdk && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+# Install SDKMAN!
+curl -s "https://get.sdkman.io" | bash
+
+# Source SDKMAN!
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# Install Java 11 (you can specify vendor, e.g., 11.0.12-open for OpenJDK)
+sdk install java ${version}
+
+# Set Java 11 as default
+sdk default java ${version}
+
+# Create symbolic links
+ln -sf "$(sdkman which java)" /usr/local/bin/java
+ln -sf "$(sdkman which javac)" /usr/local/bin/javac
+
+# Verify
+java_version=$(java -version)
+if [[ $java_version =~ "${version}" ]]; then
+	:
+else
+	echo "ERROR: java version does not match. expect \"${version}\", but actual is \"${java_version}\""
+	exit 1
+fi
+javac_version=$(javac -version)
+if [[ $javac_version =~ "${version}" ]]; then
+	:
+else
+	echo "ERROR: javac version does not match. expect \"${version}\", but actual is \"${javac_version}\""
+	exit 1
+fi
