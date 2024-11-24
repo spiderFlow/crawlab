fd1ecd11a718d88bf0218f6d13f467e075893f4d
Marvin Zhang <tikazyq@163.com>
2024-11-24 18:09:00 +0800

    ci: updated docker-init.sh

--- docker/bin/docker-init.sh
+++ docker/bin/docker-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${CRAWLAB_MASTER_NODE}" == "Y" ]; then
+if [ "${CRAWLAB_NODE_MASTER}" == "Y" ]; then
     # Replace default api path to new one
     python /app/bin/update_docker_js_api_address.py
 
