c337bf0f9ca57c1e62749dce0d1db5e32552bf6b
Marvin Zhang <tikazyq@163.com>
2024-11-24 17:47:29 +0800

    ci: updated dockerfile

--- docker/bin/docker-init.sh
+++ docker/bin/docker-init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# replace default api path to new one
-python /app/bin/update_docker_js_api_address.py
+if [ "${CRAWLAB_MASTER_NODE}" == "Y" ]; then
+    # Replace default api path to new one
+    python /app/bin/update_docker_js_api_address.py
 
+    # Start nginx
+    service nginx start
+fi
+
+# Start backend
 crawlab-server server
