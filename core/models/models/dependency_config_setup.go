79c1d5d14b9fdedb4930cfd35ed149cec05b1324
Marvin Zhang <tikazyq@163.com>
2024-12-16 21:44:03 +0800

    feat: updated dependency config setup

--- core/models/models/dependency_config_setup.go
+++ core/models/models/dependency_config_setup.go
@@ -8,6 +8,12 @@ type DependencyConfigSetup struct {
 	DependencyConfigId               primitive.ObjectID `json:"dependency_config_id" bson:"dependency_config_id"`
 	NodeId                           primitive.ObjectID `json:"node_id" bson:"node_id"`
 	Version                          string             `json:"version" bson:"version"`
+	Drivers                          []DependencyDriver `json:"versions,omitempty" bson:"versions,omitempty"`
 	Status                           string             `json:"status" bson:"status"`
 	Error                            string             `json:"error,omitempty" bson:"error,omitempty"`
+	Node                             *Node              `json:"node,omitempty" bson:"-"`
+}
+type DependencyDriver struct {
+	Name    string `json:"name" bson:"name"`
+	Version string `json:"version" bson:"version"`
 }
