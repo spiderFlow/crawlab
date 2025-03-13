43691d0d8affd8fe13218a817b32781fd5f3802d
Marvin Zhang <tikazyq>
2025-03-13 17:43:55 +0800

    refactor: enhance controller parameter handling and error responses
    - Refactored export and filter controller methods to utilize structured parameter types for improved clarity and maintainability.
    - Consolidated error handling and response generation, ensuring consistent API responses across methods.
    - Updated function signatures to eliminate unnecessary context parameters, enhancing type safety and readability.
    - Improved handling of query parameters and conditions, streamlining the logic for better performance and maintainability.

--- core/controllers/filter.go
+++ core/controllers/filter.go
@@ -4,23 +4,34 @@ import (
 	"github.com/crawlab-team/crawlab/core/entity"
 	"github.com/crawlab-team/crawlab/core/mongo"
 	"github.com/gin-gonic/gin"
+	"github.com/juju/errors"
 	"go.mongodb.org/mongo-driver/bson"
 	mongo2 "go.mongodb.org/mongo-driver/mongo"
 )
 
-func GetFilterColFieldOptions(c *gin.Context) {
-	colName := c.Param("col")
-	value := c.Param("value")
+type GetFilterColFieldOptionsParams struct {
+	Col        string `path:"col" validate:"required"`
+	Value      string `path:"value"`
+	Label      string `path:"label"`
+	Conditions string `query:"conditions" description:"Filter conditions. Format: [{\"key\":\"name\",\"op\":\"eq\",\"value\":\"test\"}]"`
+}
+
+func GetFilterColFieldOptions(_ *gin.Context, params *GetFilterColFieldOptionsParams) (response *Response[[]entity.FilterSelectOption], err error) {
+	value := params.Value
 	if value == "" {
 		value = "_id"
 	}
-	label := c.Param("label")
+	label := params.Label
 	if label == "" {
 		label = "name"
 	}
-	query := MustGetFilterQuery(c)
+
 	pipelines := mongo2.Pipeline{}
-	if query != nil {
+	if params.Conditions != "" {
+		query, err := GetFilterFromConditionString(params.Conditions)
+		if err != nil {
+			return GetErrorResponse[[]entity.FilterSelectOption](errors.Trace(err))
+		}
 		pipelines = append(pipelines, bson.D{{"$match", query}})
 	}
 	pipelines = append(
@@ -60,10 +71,11 @@ func GetFilterColFieldOptions(c *gin.Context) {
 			},
 		},
 	)
+
 	var options []entity.FilterSelectOption
-	if err := mongo.GetMongoCol(colName).Aggregate(pipelines, nil).All(&options); err != nil {
-		HandleErrorInternalServerError(c, err)
-		return
+	if err := mongo.GetMongoCol(params.Col).Aggregate(pipelines, nil).All(&options); err != nil {
+		return GetErrorResponse[[]entity.FilterSelectOption](errors.Trace(err))
 	}
-	HandleSuccessWithData(c, options)
+
+	return GetDataResponse(options)
 }
